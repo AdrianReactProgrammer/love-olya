6465e660faff983bf173e471050242857954ca2e
AdrianReactProgrammer <savchuk.adrian@gmail.com>
2022-12-21 19:20:03 +0300

    123213213123

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Timer from "./components/WhyILoveYou/Timer/Timer";
 import styles from "./App.module.css"
 import Navbar from "./components/Navbar/Navbar";
 import WhyILoveYou from "./components/WhyILoveYou/WhyILoveYou";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HowILoveYou from './components/HowILoveYou/HowILoveYou';
 
 
 
 const App = (props) => {
   return (
     <div className={styles.app} >
-      <Timer date={new Date('Saturday, 31 Dec 2022 08:50:00')}/>
+      <Timer date={new Date('Saturday, 31 Dec 2022 08:50:00')} />
       <Navbar />
-      <WhyILoveYou />
+      <Routes>
+        <Route path="/WhyILoveYou" element={<WhyILoveYou />} />
+        <Route path="/HowILoveYou" element={<HowILoveYou />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/components/HowILoveYou/HowILoveYou.jsx
@@ -0,0 +1,11 @@
+import styles from "./HowILoveYou.module.css"
+
+const HowILoveYou = (props) => {
+  return (
+    <div className={styles.howILoveYou} >
+      <h1>Как я тебя люблю?</h1>
+    </div>
+  )
+}
+
+export default HowILoveYou
