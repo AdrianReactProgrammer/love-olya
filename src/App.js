5915fbba24e5fd3167f9217ce45a69b53d122e4e
AdrianReactProgrammer <savchuk.adrian@gmail.com>
2022-12-21 18:33:17 +0300

    123123

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Timer from "./components/WhyILoveYou/Timer/Timer";
 import styles from "./App.module.css"
+import Navbar from "./components/Navbar/Navbar";
 
 
 
@@ -7,6 +8,7 @@ const App = (props) => {
   return (
     <div className={styles.app} >
       <Timer date={new Date('Saturday, 31 Dec 2022 08:50:00')}/>
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import styles from "./Navbar.module.css"
+
+const Navbar = (props) => {
+  return (
+    <div className={styles.navbar} >
+      <div>123</div>
+      <div>111</div>
+      <div>232</div>
+      <div>542</div>
+      <div>123</div>
+      <div>234</div>
+      <div>334</div>
+      <div>533</div>
+      <div>653</div>
+      <div>134</div>
+    </div>
+  )
+}
+
+export default Navbar
