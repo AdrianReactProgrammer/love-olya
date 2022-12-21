7dc168cb4e4d6100be49d362a5a586cbb97c168b
AdrianReactProgrammer <savchuk.adrian@gmail.com>
2022-12-21 19:03:20 +0300

    asdas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Timer from "./components/WhyILoveYou/Timer/Timer";
 import styles from "./App.module.css"
 import Navbar from "./components/Navbar/Navbar";
+import WhyILoveYou from "./components/WhyILoveYou/WhyILoveYou";
 
 
 
@@ -9,6 +10,7 @@ const App = (props) => {
     <div className={styles.app} >
       <Timer date={new Date('Saturday, 31 Dec 2022 08:50:00')}/>
       <Navbar />
+      <WhyILoveYou />
     </div>
   )
 }
---
+++ src/components/WhyILoveYou/WhyILoveYou.jsx
@@ -0,0 +1,11 @@
+import styles from "./WhyILoveYou.module.css"
+
+const WhyILoveYou = (props) => {
+  return (
+    <div className={styles.whyILoveYou} >
+      <h1>Почему я тебя люблю?</h1>
+    </div>
+  )
+}
+
+export default WhyILoveYou
